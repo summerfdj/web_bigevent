faae979e2dcebe8012a9c4e8b6261a2cfa1121c8
fdj <243519605@qq.com>
2022-04-02 14:30:34 +0800

    完成了user

---
+++ assets/js/user/user_avatar.js
@@ -0,0 +1,54 @@
+$(function() {
+    // 1.1 获取裁剪区域的 DOM 元素
+    var $image = $('#image')
+        // 1.2 配置选项
+    const options = {
+        // 纵横比
+        aspectRatio: 1,
+        // 指定预览区域
+        preview: '.img-preview'
+    }
+
+    // 1.3 创建裁剪区域
+    $image.cropper(options)
+
+    // 上传
+    $('#btnChooseImg').on('click', function() {
+        $('#file').click()
+    })
+
+    $('#file').on('change', function() {
+        let files = $('#file')[0].files
+        if (files.length === 0) {
+            return layui.layer.msg('请选择图片')
+        }
+        let imgURL = URL.createObjectURL(files[0])
+        $image
+            .cropper('destroy') // 销毁旧的裁剪区域
+            .attr('src', imgURL) // 重新设置图片路径
+            .cropper(options)
+    })
+
+    $('#btnUnload').on('click', function() {
+        var dataURL = $image
+            .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+                width: 100,
+                height: 100
+            })
+            .toDataURL('image/png')
+        $.ajax({
+            method: 'POST',
+            url: '/my/update/avatar',
+            data: {
+                avatar: dataURL,
+            },
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layui.layer.msg(res.message)
+                }
+                layui.layer.msg(res.message)
+                window.parent.getUserInfo()
+            }
+        })
+    })
+})
